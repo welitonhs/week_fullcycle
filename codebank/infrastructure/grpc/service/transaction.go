06f4a47a5dce717bd40fd894cf62911351574abd
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2021-07-06 22:16:34 -0300

    Second Class

---
+++ codebank/infrastructure/grpc/service/transaction.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/welitonhs/fccodebank/dto"
+	"github.com/welitonhs/fccodebank/infrastructure/grpc/pb"
+	"github.com/welitonhs/fccodebank/usecase"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type TransactionService struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+	pb.UnimplementedPaymentServiceServer
+}
+
+func NewTransactionService() *TransactionService {
+	return &TransactionService{}
+}
+
+func (t *TransactionService) Payment(ctx context.Context, in *pb.PaymentRequest) (*empty.Empty, error) {
+	transactionDto := dto.Transaction{
+		Name:            in.GetCreditCard().GetName(),
+		Number:          in.CreditCard.GetNumber(),
+		ExpirationMonth: in.GetCreditCard().GetExpirationMonth(),
+		ExpirationYear:  in.GetCreditCard().ExpirationYear,
+		CVV:             in.GetCreditCard().Cvv,
+		Amount:          in.GetAmount(),
+		Store:           in.GetStore(),
+		Description:     in.GetDescription(),
+	}
+	transaction, err := t.ProcessTransactionUseCase.ProcessTransaction(transactionDto)
+	if err != nil {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, err.Error())
+	}
+
+	if transaction.Status != "approved" {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, "transaction rejected by the bank")
+	}
+
+	return &empty.Empty{}, nil
+}
